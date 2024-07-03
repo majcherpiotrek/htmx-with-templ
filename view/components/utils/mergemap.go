5fa72f38c1c27884f9107dc2a3b37c96b95adfb0
Piotr Majcher <majcherpiotr.dev@gmail.com>
2024-07-03 12:00:26 +0100

    improved map merging functions

--- view/components/utils/mergemap.go
+++ view/components/utils/mergemap.go
@@ -2,22 +2,26 @@ package utils
 
 import "github.com/a-h/templ"
 
-func MergeMap[K comparable, T any](base, toMerge *map[K]T) map[K]T {
+func MergeMap[K comparable, T any](maps ...*map[K]T) map[K]T {
 	result := make(map[K]T)
 
-	for k, v := range *base {
-		result[k] = v
-	}
-
-	for k, v := range *toMerge {
-		result[k] = v
+	for _, toMerge := range maps {
+		for k, v := range *toMerge {
+			result[k] = v
+		}
 	}
 
 	return result
 }
 
-func MergeAttributes(base, toMerge *templ.Attributes) templ.Attributes {
-	merged := MergeMap((*map[string]any)(base), (*map[string]any)(toMerge))
+func MergeAttributes(attrs ...*templ.Attributes) templ.Attributes {
+	attrsAsMaps := make([]*map[string]any, len(attrs))
+
+	for i, toMerge := range attrs {
+		attrsAsMaps[i] = (*map[string]any)(toMerge)
+	}
+
+	merged := MergeMap(attrsAsMaps...)
 
 	return templ.Attributes(merged)
 }
